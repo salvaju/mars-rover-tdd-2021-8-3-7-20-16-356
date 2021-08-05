92884e5e0050079c76cd03860a41700b38a37dea
Juan Carlos Salvador <salvaju@oocl.com>
2021-08-05 20:15:39 +0800

    [MarsRover][Juan Carlos Salvador] test: add test should_return_locationX_0_locationY_0_direction_W_when_execute_command_given_locationX_0_locationY_0_direction_S_and_R

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -91,4 +91,21 @@ public class MarsRoverTest {
 
     }
 
+    @Test
+    void should_return_locationX_0_locationY_0_direction_W_when_execute_command_given_locationX_0_locationY_0_direction_S_and_R(){
+
+        //given
+        MarsRover marsRover = new MarsRover(new RoverStatus(0, 0, "S"));
+        RoverStatus expectedRoverStatus = new RoverStatus(0, 0, "W");
+
+        //when
+        marsRover.executeCommand("R");
+
+        //then
+        assertEquals(expectedRoverStatus.getLocationX(), marsRover.getRoverStatus().getLocationX());
+        assertEquals(expectedRoverStatus.getLocationY(), marsRover.getRoverStatus().getLocationY());
+        assertEquals(expectedRoverStatus.getDirection(), marsRover.getRoverStatus().getDirection());
+
+    }
+
 }
